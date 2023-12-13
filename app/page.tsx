78597b2fb4035bfee746d2d8f4741b6a13d3a79f
Juncker Maxime <82824938+Maxime-juncker@users.noreply.github.com>
2023-12-13 15:52:59 +0100

    vroom

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,92 @@
 import Image from 'next/image'
+import SkillCard from './Components/SkillCard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <body className="App">
+              {/* <Navbar/> */}
+        
+                <section className='Hero' id='Hero'>
+                  {/* <PointLight left={10} top={60} backgroundColor={violet}/>
+                  <PointLight left={80} top={45} backgroundColor={violet}/>
+                  <PointLight left={25} top={10} backgroundColor={violet}/> */}
+        
+                  <h1>JUNCKER <br /> MAXIME</h1>
+                  <h3>SOFTWARE DEVELOPPER</h3>
+                </section>
+        
+                <section className='About' id='About'>
+                  {/* <PointLight left={0} backgroundColor={blue}/>
+                  <PointLight left={65} top={20} backgroundColor={blue}/> */}
+                  <div className="Presentation">
+                    <h1>ABOUT ME</h1>
+                    <p>
+                      I’m a frensh student who’s looking into software developpement, 
+                      I also know web and game developpement. <br /><br />
+                      I use object oriented language and mainly 
+                      use c++ and c# language to create some small app 
+                      for android or windows. <br /><br />
+                      I’m also looking into creating 
+                      simulation and machine learning stuff.
+                    </p>
+                  </div>
+                  <div className="Skills">
+                    <h1>SKILLS</h1>
+                    <div className="Card-conterner">
+                      <SkillCard content={"C#"} src={"../Images/Cards/cpp.svg"}/>
+                      <SkillCard content={"C++"} src={"../Images/Cards/cpp.svg"}/>
+                      <SkillCard content={"CSS"} src={"../Images/Cards/css.svg"}/>
+                      <SkillCard content={"PYTHON"} src={"../Images/Cards/html.svg"}/>
+                      <SkillCard content={"UNITY"} src={"../Images/Cards/unity.svg"}/>
+                      <SkillCard content={"UNREAL"} src={"../Images/Cards/unreal.svg"}/>
+                      <SkillCard content={"BLENDER"} src={"../Images/Cards/blender.svg"}/>
+                      <SkillCard content={"VS CODE"} src={"../Images/Cards/visual-studio-code.svg"}/>
+                      <SkillCard content={"VISUAL STUDIO"} src={"../Images/Cards/visual-studio.svg"}/>
+                      <SkillCard content={"RIDER"} src={"../Images/Cards/jetbrains.svg"}/>
+                      <SkillCard content={"GITHUB"} src={"../Images/Cards/github.svg"}/>
+                      <SkillCard content={"XAMARIN"} src={"../Images/Cards/xamarin.svg"}/>
+                      <SkillCard content={"AVALONIA UI"} src={"../Images/Cards/avalonia.svg"}/>
+                    </div>
+                  </div>
+                </section>
+        
+                <section className='Work' id='Work'>
+                  <h1>WORK</h1>
+                  <h2>MY LATEST PROJECT</h2>
+        
+                  {/* <ProjectCard src={require("../Images/Projects/Maximatron/Maximatron-screen.png")}
+                               title="The Maximatron" description="Desktop development"
+                               logo={require("../Images/Projects/Maximatron/Maximatron-logo.png")}
+                               nav={"/Maximatron"}/>
+                  <ProjectCard src={require("../Images/Projects/HumanSI/HumanSI-screen.png")}
+                               title="HumanSI" 
+                               description="Human Simulation"
+                               logo={require("../Images/Projects/HumanSI/HumanSI-logo.png")}
+                               nav={"/HumanSI"}/>
+                  <ProjectCard src={require("../Images/Projects/Pronout/Pronoute-screen.png")}
+                               title="Pronout" 
+                               description="Mobile App"
+                               logo={require("../Images/Projects/Pronout/Pronoute_logo.png")}
+                               nav={"/Pronout"}/> */}
+                  {/* <PointLight left={70} top={20} backgroundColor={red}/>
+                  <PointLight left={10} top={5} backgroundColor={red}/>
+                  <PointLight left={65} top={55} backgroundColor={red}/>
+                  <PointLight left={0} top={70} backgroundColor={red}/>
+                  <PointLight left={80} top={80} backgroundColor={red}/>
+                  <PointLight left={80} top={150} backgroundColor={green}/>
+                  <PointLight left={0} top={100} backgroundColor={green}/>
+                  <PointLight left={10} top={150} backgroundColor={green}/>                */}
+        
+                </section>
+        
+                <section id='Contact' className='Contact'>
+                  <h1>CONTACT</h1>
+                  <section>
+                    <article className='Contact-card'>
+                      {/* <ContactForm/> */}
+                    </article>
+                  </section>
+                </section>
+            </body>  
   )
 }
